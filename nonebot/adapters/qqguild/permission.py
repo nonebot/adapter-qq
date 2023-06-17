9ec83b4053b7105e877ec265e2bf574faa620862
17TheWord <54731914+17TheWord@users.noreply.github.com>
2023-06-18 00:51:44 +0800

    Create permission.py
    增加频道管理员与频道主的权限

---
+++ nonebot/adapters/qqguild/permission.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from nonebot.permission import Permission
+from .event import AtMessageCreateEvent, MessageCreateEvent
+
+
+async def _guild_admin(event: Union[AtMessageCreateEvent, MessageCreateEvent]) -> bool:
+    return 2 in event.member.roles
+
+
+async def _guild_owner(event: Union[AtMessageCreateEvent, MessageCreateEvent]) -> bool:
+    return 4 in event.member.roles
+
+
+GUILD_ADMIN: Permission = Permission(_guild_admin)
+"""匹配任意频道管理员群聊消息类型事件"""
+GUILD_OWNER: Permission = Permission(_guild_owner)
+"""匹配任意频道群主群聊消息类型事件"""
+
+__all__ = [
+    "GUILD_ADMIN",
+    "GUILD_OWNER",
+]
