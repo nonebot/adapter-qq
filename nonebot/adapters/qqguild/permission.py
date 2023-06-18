b698d8cf6de73e3ee9d71ec00dbb20dd19f35d13
17TheWord <54731914+17TheWord@users.noreply.github.com>
2023-06-18 14:48:31 +0800

    Update permission.py
    新增子频道管理员权限

--- nonebot/adapters/qqguild/permission.py
+++ nonebot/adapters/qqguild/permission.py
@@ -1,8 +1,11 @@
 from typing import Union
 
 from nonebot.permission import Permission
-
 from .event import MessageCreateEvent, AtMessageCreateEvent
+
+
+async def _guild_channel_admin(event: Union[AtMessageCreateEvent, MessageCreateEvent]) -> bool:
+    return 5 in event.member.roles
 
 
 async def _guild_admin(event: Union[AtMessageCreateEvent, MessageCreateEvent]) -> bool:
@@ -13,12 +16,15 @@ async def _guild_owner(event: Union[AtMessageCreateEvent, MessageCreateEvent]) -
     return 4 in event.member.roles
 
 
+GUILD_CHANNEL_ADMIN: Permission = Permission(_guild_channel_admin)
+"""匹配任意子频道管理员聊消息类型事件"""
 GUILD_ADMIN: Permission = Permission(_guild_admin)
-"""匹配任意频道管理员群聊消息类型事件"""
+"""匹配任意频道管理员聊消息类型事件"""
 GUILD_OWNER: Permission = Permission(_guild_owner)
-"""匹配任意频道群主群聊消息类型事件"""
+"""匹配任意频道群主群消息类型事件"""
 
 __all__ = [
+    "GUILD_CHANNEL_ADMIN",
     "GUILD_ADMIN",
     "GUILD_OWNER",
 ]
