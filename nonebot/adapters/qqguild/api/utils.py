c53a3086486666c63d1ecd389c98936df1c2590f
iRis7656 <122857198+iRis7656@users.noreply.github.com>
2023-08-31 16:20:59 +0800

    :bug: Fix: 修复在使用 file_image 时，其他字段失效 (#50)
    Co-authored-by: Mix <32300164+mnixry@users.noreply.github.com>
    Co-authored-by: Ju4tCode <42488585+yanyongyu@users.noreply.github.com>

--- nonebot/adapters/qqguild/api/utils.py
+++ nonebot/adapters/qqguild/api/utils.py
@@ -8,19 +8,20 @@ def parse_send_message(data: Dict[str, Any]) -> Dict[str, Any]:
     model_data = MessageSend(**data).dict(exclude_none=True)
     if file_image := model_data.pop("file_image", None):
         # 使用 multipart/form-data
-        data_: Dict[str, Any] = {"file_image": ("file_image", file_image)}
+        multipart_files: Dict[str, Any] = {"file_image": ("file_image", file_image)}
+        multipart_data: Dict[str, Any] = {}
         for k, v in model_data.items():
             if isinstance(v, (dict, list)):
                 # 当字段类型为对象或数组时需要将字段序列化为 JSON 字符串后进行调用
                 # https://bot.q.qq.com/wiki/develop/api/openapi/message/post_messages.html#content-type
-                data_[k] = (
-                    None,
+                multipart_files[k] = (
+                    k,
                     json.dumps({k: v}).encode("utf-8"),
                     "application/json",
                 )
             else:
-                data_[k] = (None, v.encode("utf-8"), "text/plain")
-        params = {"files": data_}
+                multipart_data[k] = v
+        params = {"files": multipart_files, "data": multipart_data}
     else:
         params = {"json": model_data}
     return params
