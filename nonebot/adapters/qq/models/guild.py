bf689ed2891551dc201babd0a2bfc7cf095be2cf
mobyw <mobyw66@gmail.com>
2023-10-02 19:38:14 +0800

    :bug: Fix: 修复子频道字段以适配子频道列表中的子频道分组项

--- nonebot/adapters/qq/models/guild.py
+++ nonebot/adapters/qq/models/guild.py
@@ -73,8 +73,8 @@ class Channel(BaseModel):
     position: int
     parent_id: Optional[str] = None
     owner_id: Optional[str] = None
-    private_type: Union[PrivateType, int]
-    speak_permission: Union[SpeakPermission, int]
+    private_type: Union[PrivateType, int, None] = None
+    speak_permission: Union[SpeakPermission, int, None] = None
     application_id: Optional[str] = None
     permissions: Optional[int] = None
 
