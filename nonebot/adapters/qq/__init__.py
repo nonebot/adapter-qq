8e876ac6330b3ef534bb551fdbfdd507e97307bb
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-10-14 10:15:18 +0000

    :bug: re-export exceptions

--- nonebot/adapters/qq/__init__.py
+++ nonebot/adapters/qq/__init__.py
@@ -4,4 +4,12 @@ from .bot import Bot as Bot
 from .utils import log as log
 from .adapter import Adapter as Adapter
 from .message import Message as Message
+from .exception import ActionFailed as ActionFailed
+from .exception import NetworkError as NetworkError
 from .message import MessageSegment as MessageSegment
+from .exception import AuditException as AuditException
+from .exception import NoLogException as NoLogException
+from .exception import ApiNotAvailable as ApiNotAvailable
+from .exception import QQAdapterException as QQAdapterException
+from .exception import RateLimitException as RateLimitException
+from .exception import UnauthorizedException as UnauthorizedException
