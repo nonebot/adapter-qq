1287d9e5466f01f7d3dd177b35bd6989d69f6c34
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-10-15 07:23:41 +0000

    :bug: fix event export

--- nonebot/adapters/qq/__init__.py
+++ nonebot/adapters/qq/__init__.py
@@ -2,8 +2,10 @@ from .event import *
 from .permission import *
 from .bot import Bot as Bot
 from .utils import log as log
+from .utils import escape as escape
 from .adapter import Adapter as Adapter
 from .message import Message as Message
+from .utils import unescape as unescape
 from .exception import ActionFailed as ActionFailed
 from .exception import NetworkError as NetworkError
 from .message import MessageSegment as MessageSegment
