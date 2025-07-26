0317081fb2c9e9cfeea91e5a234688b4bf308a21
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2025-07-26 04:10:17 +0000

    :arrow_up: upgrade dependencies

--- nonebot/adapters/qq/models/qq.py
+++ nonebot/adapters/qq/models/qq.py
@@ -117,14 +117,14 @@ class MessagePromptKeyboard(BaseModel):
 
 
 class MessageStream(BaseModel):
-    state: Literal[
-        1, 10, 11, 20
-    ]  # 1: 正文生成中, 10: 正文生成结束, 11: 引志消息生成中, 20: 引导消息生成结束。
-    id: Optional[str] = None  # 第一条不用填写，第二条需要填写第一个分片返回的 msgID
-    index: int  # 从 1 开始
-    reset: Optional[bool] = (
-        None  # 只能用于流式消息没有发送完成时，reset 时 index 需要从 0 开始，需要填写流式 id
-    )
+    state: Literal[1, 10, 11, 20]
+    """1: 正文生成中, 10: 正文生成结束, 11: 引志消息生成中, 20: 引导消息生成结束。"""
+    id: Optional[str] = None
+    """第一条不用填写，第二条需要填写第一个分片返回的 msgID"""
+    index: int
+    """从 1 开始"""
+    reset: Optional[bool] = None
+    """只能用于流式消息没有发送完成时，reset 时 index 需要从 0 开始，需要填写流式 id"""
 
 
 __all__ = [
